b46d0927b9089627ae94f4f2fb1cfeee67d52ed4
Vlad Beliaiev <vladislav.belyaev.93@gmail.com>
2016-11-17 21:29:28 +0200

    Refactor tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email,
+                                 :password,
+                                 :password_confirmation,
+                                 :first_name,
+                                 :last_name)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has a valid factory" do
-    expect(FactoryGirl.build(:user)).to be_valid
-  end
-
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_presence_of(:password) }
 
-  context do
-    let(:user) { FactoryGirl.create(:user) }
+  context 'when name is present' do
+    let(:user) { create(:user, first_name: 'John', last_name: 'Doe') }
 
     it 'should return correct display name' do
-      expect(user.display_name).to eq (user.first_name + ' ' + user.last_name)
+      expect(user.display_name).to eq('John Doe')
     end
   end
 
-  context do
-    let(:user) { FactoryGirl.create(:user,
-                                    first_name: '',
-                                    last_name: '') }
+  context 'when name is blank' do
+    let(:user) { create(:user, first_name: '', last_name: '') }
 
     it 'should return "User" as display name' do
       expect(user.display_name).to eq('User')
