9b16385e8903d2f60d1c6c23b934faeb18eb16fa
Vladislav <vladislav.belyaev.93@gmail.com>
2016-11-20 23:46:50 +0200

    Fix footer and add firs_tname/last_name length. [Delivers #134593519 #134594839 #133005047] (#5)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   validates_presence_of :password
   validates_confirmation_of :password
 
+  validates :first_name, length: { minimum: 2, maximum: 25 }, allow_blank: true
+  validates :last_name, length: { minimum: 2, maximum: 25 }, allow_blank: true
+
   def display_name
     return 'User' if first_name.blank? && last_name.blank?
     "#{first_name} #{last_name}"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_presence_of(:password) }
-
+  it { is_expected.to validate_length_of(:first_name).is_at_most(25).is_at_least(2) }
+  it { is_expected.to validate_length_of(:last_name).is_at_most(25).is_at_least(2) }
+  
   context 'when name is present' do
     let(:user) { create(:user, first_name: 'John', last_name: 'Doe') }
 
