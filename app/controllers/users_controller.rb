0defe176ff963d4648558ebdd7657078e8de5a82
Oksana Dushenkovskaya <oksanka-blaze@users.noreply.github.com>
2016-12-10 00:07:35 +0200

    Add ability to switch languages, add about us page, add ability to de… (#11) (#12)
    * Add ability to switch languages, add about us page, add ability to delete own posts. [Delivers #133006445 #133006553 #133006489]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, notice: 'Signed up!'
+      redirect_to root_url, notice: t('flash.users.notice.signed_up')
     else
       render :new
     end
